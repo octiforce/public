7e94b31ef73d1dc95cd0f1896e75dbad6b835f4d
octiforce <businessbrianwilson@gmail.com>
2026-08-26 13:59:31 -0500

    Second test.

--- python/redsolid/src/redsolid/strings.py
+++ python/redsolid/src/redsolid/strings.py
@@ -199,6 +199,14 @@ def str_to_list(
 def str_from_mapping(
         items: Mapping[Any, Any], *, sep: str = "\n", link: str = ":"
 ) -> str:
+    """
+    Convert any Mapping into a simple separated string.
+
+    The returned string contains all key/value entries converted to
+    strings.  Each key and value is separated by a link string
+    (default is ":"), then each entry is separated by a separator
+    string (default is newline).
+    """
     _validate_not_empty(sep, link)
     return sep.join(f"{key}{link}{value}" for key, value in items.items())
 
